74ad7e77da8fc406e111356358abeaa908a6c384
Jason Barrie Morley <jason.morley@inseven.co.uk>
2018-12-18 08:29:51 -0800

    Relative images and secure fetch.

--- ios/Stuff/ViewController.swift
+++ ios/Stuff/ViewController.swift
@@ -13,6 +13,10 @@ class Cell: UICollectionViewCell {
     @IBOutlet weak var imageView: UIImageView!
 }
 
+enum OpenGraphError: Error {
+    case invalidArgument(message: String)
+}
+
 class ViewController: UICollectionViewController {
 
     var posts: [Post]!
@@ -44,6 +48,64 @@ class ViewController: UICollectionViewController {
         return self.posts.count
     }
 
+    func image(for url: URL, completion: @escaping (Result<UIImage>) -> Void) {
+
+        // Force HTTPS.
+        guard var components = URLComponents(string: url.absoluteString) else {
+            completion(.failure(OpenGraphError.invalidArgument(message: "Unable to create URL components")))
+            return
+        }
+
+        components.scheme = "https"
+
+        guard let secureUrl = components.url else {
+            completion(.failure(OpenGraphError.invalidArgument(message: "Unable to create secure URL")))
+            return
+        }
+
+        let task = URLSession.shared.dataTask(with: secureUrl) { (data, response, error) in
+            guard let data = data else {
+                completion(.failure(OpenGraphError.invalidArgument(message: "Unable to get contents of URL for cell")))
+                return
+            }
+            guard let doc = TFHpple(htmlData: data) else {
+                completion(.failure(OpenGraphError.invalidArgument(message: "Unable to parse the data")))
+                return
+            }
+            guard let elements = doc.search(withXPathQuery: "//meta[@property='og:image']/@content") as? [TFHppleElement] else {
+                completion(.failure(OpenGraphError.invalidArgument(message: "Unable to find open graph image tag")))
+                return
+            }
+            guard let element = elements.first else {
+                completion(.failure(OpenGraphError.invalidArgument(message: "Unable to find open graph image tag")))
+                return
+            }
+            guard let content = element.content else {
+                completion(.failure(OpenGraphError.invalidArgument(message: "Image tag had no content")))
+                return
+            }
+            guard let components = URLComponents(string: content) else {
+                completion(.failure(OpenGraphError.invalidArgument(message: "Unable to create URL components")))
+                return
+            }
+            guard let imageURL = components.url(relativeTo: secureUrl) else {
+                completion(.failure(OpenGraphError.invalidArgument(message: "Invalid image URL")))
+                return
+            }
+            guard let imageData = try? Data.init(contentsOf: imageURL) else {
+                completion(.failure(OpenGraphError.invalidArgument(message: "Unable to download image")))
+                return
+            }
+            guard let image = UIImage.init(data: imageData) else {
+                completion(.failure(OpenGraphError.invalidArgument(message: "Unable to construct image")))
+                return
+            }
+            completion(.success(image))
+        }
+        task.resume()
+
+    }
+
     override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "Cell", for: indexPath) as! Cell
         let post = self.posts[indexPath.row]
@@ -55,53 +117,23 @@ class ViewController: UICollectionViewController {
             return cell
         }
 
-        // Download the contents of the page.
-        let task = URLSession.shared.dataTask(with: url) { (data, response, error) in
-            guard let data = data else {
-                print("Unable to get contents of URL for cell")
-                return
-            }
-            guard let doc = TFHpple(htmlData: data) else {
-                print("Unable to parse the data")
-                return
-            }
-            guard let elements = doc.search(withXPathQuery: "//meta[@property='og:image']/@content") as? [TFHppleElement] else {
-                print("Unable to find open graph image tag")
-                return
-            }
-            guard let element = elements.first else {
-                print("Unable to find open graph image tag")
-                return
-            }
-            guard let content = element.content else {
-                print("Image tag had no content")
-                return
-            }
-            guard let imageURL = URL(string: content) else {
-                print("Invalid URL")
-                return
-            }
-            guard let imageData = try? Data.init(contentsOf: imageURL) else {
-                print("Unable to download image")
-                return
-            }
-            guard let image = UIImage.init(data: imageData) else {
-                print("Unable to construct image")
-                return
-            }
-            print("Successfully downloaded image")
-            DispatchQueue.main.async { [weak self] in  // TODO: Sync?
+        image(for: url) { (result) in
+            DispatchQueue.main.async { [weak self] in
                 guard let self = self else {
                     return
                 }
-                // TODO: What happens if the cell locations have changed?
-                guard let cell = self.collectionView.cellForItem(at: indexPath) as? Cell else {
-                    return
+                switch (result) {
+                case .success(let image):
+                    // TODO: What happens if the cell locations have changed?
+                    guard let cell = self.collectionView.cellForItem(at: indexPath) as? Cell else {
+                        return
+                    }
+                    cell.imageView.image = image
+                case .failure(let error):
+                    print("Failed to download image for URL '\(url)' with error '\(error)'")
                 }
-                cell.imageView.image = image
             }
         }
-        task.resume()
 
         return cell
     }

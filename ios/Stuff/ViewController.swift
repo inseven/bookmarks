adfbe5eb2bf5444a08c8c8e100698087b3444d99
Jason Barrie Morley <jason.morley@inseven.co.uk>
2020-05-26 12:59:44 -0700

    Better layout.

--- ios/Stuff/ViewController.swift
+++ ios/Stuff/ViewController.swift
@@ -83,7 +83,27 @@ class ViewController: UIViewController  {
 
         collectionView.dataSource = dataSource
         navigationItem.searchController = searchController
+        collectionView.collectionViewLayout = createLayout()
     }
+
+    private func createLayout() -> UICollectionViewLayout {
+        let itemSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1/3),
+                                              heightDimension: .fractionalHeight(1))
+        let spacing: CGFloat = 20.0
+        let item = NSCollectionLayoutItem(layoutSize: itemSize)
+        item.contentInsets = NSDirectionalEdgeInsets(top: 0, leading: 0, bottom: 0, trailing: spacing)
+        let groupSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0),
+                                               heightDimension: .absolute(280))
+        let group = NSCollectionLayoutGroup.horizontal(layoutSize: groupSize,
+                                                       subitems: [item, item])
+        group.contentInsets = NSDirectionalEdgeInsets(top: 0, leading: spacing, bottom: 0, trailing: 0)
+        let section = NSCollectionLayoutSection(group: group)
+        section.interGroupSpacing = spacing
+        section.contentInsets = NSDirectionalEdgeInsets(top: spacing, leading: 0.0, bottom: 0.0, trailing: 0.0)
+
+        let layout = UICollectionViewCompositionalLayout(section: section)
+        return layout
+     }
 
     func update() {
         store.identifiers(filter: searchController.searchBar.searchTextField.text) { (result) in

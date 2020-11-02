12468e4b6a5bd2ba55156bff1207d79c7c6656c5
Jason Barrie Morley <jbmorley@mac.com>
2020-11-02 14:31:57 -0800

    Remove unused code.

--- ios/Stuff/Interface/SettingsView.swift
+++ ios/Stuff/Interface/SettingsView.swift
@@ -56,10 +56,3 @@ struct SettingsView: View {
         })
     }
 }
-
-struct SettingsView_Previews: PreviewProvider {
-
-    static var previews: some View {
-        SettingsView(settings: Settings())
-    }
-}

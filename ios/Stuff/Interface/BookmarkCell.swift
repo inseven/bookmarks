b0ddb3264fc7bce9f55ac3051b27c9c84e64fb89
Jason Barrie Morley <jbmorley@mac.com>
2021-03-31 16:23:04 -0700

    Remove the sidebar With the current implementation, the tags are unusable and distact from the other functionality of the app (which still needs performance tuning). This change removes the side bar to allow for focus on other aspects. It includes a drive-by fix to remove the fade animation when showing the thumbnails (also slow at the moment).

---
+++ ios/Stuff/Interface/BookmarkCell.swift
@@ -0,0 +1,99 @@
+// Copyright (c) 2020-2021 Jason Barrie Morley
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
+import Combine
+import SwiftUI
+
+struct BookmarkCell: View {
+
+    var item: Item
+
+    @Environment(\.manager) var manager: BookmarksManager
+    @State var image: UIImage?
+    @State var publisher: AnyCancellable?
+
+    var title: String {
+        if !item.title.isEmpty {
+            return item.title
+        } else if !item.url.lastPathComponent.isEmpty && item.url.lastPathComponent != "/" {
+            return item.url.lastPathComponent
+        } else if let host = item.url.host {
+            return host
+        } else {
+            return "Unknown"
+        }
+    }
+
+    var thumbnail: some View {
+        ZStack {
+            Text(title)
+                .lineLimit(3)
+                .multilineTextAlignment(.center)
+                .frame(minWidth: 0, maxWidth: .infinity, minHeight: 0, maxHeight: .infinity, alignment: .center)
+                .padding()
+                .background(Color(UIColor.tertiarySystemBackground))
+            if let image = image {
+                Image(uiImage: image)
+                    .resizable()
+                    .aspectRatio(contentMode: .fill)
+                    .frame(minWidth: 0, maxWidth: .infinity)
+                    .clipped()
+                    .background(Color.white)
+            }
+        }
+    }
+
+    var body: some View {
+        VStack(alignment: .leading) {
+            thumbnail
+                .frame(height: 100)
+                .clipped()
+            VStack(alignment: .leading) {
+                Text(title)
+                    .lineLimit(1)
+                Text(item.url.host ?? "Unknown")
+                    .lineLimit(1)
+                    .foregroundColor(.secondary)
+            }
+            .frame(minWidth: /*@START_MENU_TOKEN@*/0/*@END_MENU_TOKEN@*/, maxWidth: .infinity, alignment: .leading)
+            .padding()
+        }
+        .background(Color(UIColor.secondarySystemBackground))
+        .cornerRadius(10)
+        .onAppear {
+            publisher = manager.thumbnailManager.thumbnail(for: item)
+                .receive(on: DispatchQueue.main)
+                .sink(receiveCompletion: { (completion) in
+                    if case .failure(let error) = completion {
+                        print("Failed to download thumbnail with error \(error)")
+                    }
+                }, receiveValue: { image in
+                    self.image = image
+                })
+        }
+        .onDisappear {
+            guard let publisher = publisher else {
+                return
+            }
+            publisher.cancel()
+        }
+    }
+
+}
---
+++ ios/Stuff/Interface/BookmarksView.swift
@@ -0,0 +1,89 @@
+// Copyright (c) 2020-2021 Jason Barrie Morley
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
+import SwiftUI
+
+struct BookmarksView: View {
+
+    enum SheetType {
+        case settings
+    }
+
+    @Environment(\.manager) var manager: BookmarksManager
+    @ObservedObject var store: Store
+    var title: String
+    var tag: String?
+
+    @State var sheet: SheetType?
+    @State var search = ""
+
+    var items: [Item] {
+        store.rawItems.filter {
+            (tag == nil || $0.tags.contains(tag!)) &&
+            search.isEmpty ||
+                $0.title.localizedStandardContains(search) ||
+                $0.url.absoluteString.localizedStandardContains(search) ||
+                $0.tags.contains(where: { $0.localizedStandardContains(search) } )
+        }
+    }
+
+    var body: some View {
+        VStack {
+            HStack {
+                TextField("Search", text: $search)
+                    .modifier(SearchBoxModifier(text: $search))
+                    .padding()
+            }
+            ScrollView {
+                LazyVGrid(columns: [GridItem(.adaptive(minimum: 160), spacing: 16)], spacing: 16) {
+                    ForEach(items) { item in
+                        BookmarkCell(item: item)
+                            .onTapGesture {
+                                UIApplication.shared.open(item.url)
+                            }
+                            .contextMenu(ContextMenu(menuItems: {
+                                Button("Share") {
+                                    print("Share")
+                                    print(item.identifier)
+                                }
+                            }))
+                    }
+                }
+                .padding()
+            }
+        }
+        .sheet(item: $sheet) { sheet in
+            switch sheet {
+            case .settings:
+                NavigationView {
+                    SettingsView(settings: manager.settings)
+                }
+            }
+        }
+        .navigationBarTitle(title, displayMode: .inline)
+        .navigationBarItems(leading: Button(action: {
+            sheet = .settings
+        }) {
+            Image(systemName: "gearshape")
+                .foregroundColor(.accentColor)
+        })
+    }
+
+}

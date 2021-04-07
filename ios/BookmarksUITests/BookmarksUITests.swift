16ca146539edce61e3d9250f3252c71af00e4644
Jason Barrie Morley <jbmorley@mac.com>
2021-04-06 17:03:53 -0700

    Rename the UI tests.

--- ios/BookmarksUITests/BookmarksUITests.swift
+++ ios/BookmarksUITests/BookmarksUITests.swift
@@ -20,7 +20,7 @@
 
 import XCTest
 
-class StuffUITests: XCTestCase {
+class BookmarksUITests: XCTestCase {
 
     override func setUp() {
         // Put setup code here. This method is called before the invocation of each test method in the class.

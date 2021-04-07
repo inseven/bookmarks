1c288178fbd2b8d485551c70636e22a87f6cd274
Jason Barrie Morley <jbmorley@mac.com>
2021-04-07 11:00:01 -0700

    Clean up the header.

--- core/BookmarksCore/BookmarksCore.h
+++ core/BookmarksCore/BookmarksCore.h
@@ -20,12 +20,5 @@
 
 #import <Foundation/Foundation.h>
 
-//! Project version number for BookmarksCore.
 FOUNDATION_EXPORT double BookmarksCoreVersionNumber;
-
-//! Project version string for BookmarksCore.
 FOUNDATION_EXPORT const unsigned char BookmarksCoreVersionString[];
-
-// In this header, you should import all the public headers of your framework using statements like #import <BookmarksCore/PublicHeader.h>
-
-

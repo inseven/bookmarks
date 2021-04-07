b98890017f240c1e6d2b0a648d6a962f95dc90f7
Jason Barrie Morley <jbmorley@mac.com>
2021-04-07 11:25:05 -0700

    Move the NSCoder extensions into BookmarksCore.framework.

--- core/BookmarksCore/Extensions/NSCoder.swift
+++ core/BookmarksCore/Extensions/NSCoder.swift
@@ -22,15 +22,15 @@ import Foundation
 
 extension NSCoder {
 
-    func decodeString(forKey key: String) -> String? {
+    public func decodeString(forKey key: String) -> String? {
         return decodeObject(of: NSString.self, forKey: key) as String?
     }
 
-    func decodeUrl(forKey key: String) -> URL? {
+    public func decodeUrl(forKey key: String) -> URL? {
         return decodeObject(of: NSURL.self, forKey: key) as URL?
     }
 
-    func decodeDate(forKey key: String) -> Date? {
+    public func decodeDate(forKey key: String) -> Date? {
         return decodeObject(of: NSDate.self, forKey: key) as Date?
     }
 

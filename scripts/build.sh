d9297898c2f64d9be5e2004e14888bb75ab99ebf
Jason Barrie Morley <jbmorley@mac.com>
2021-05-02 09:20:10 -0700

    ci(macOS): Set up automatic macOS build and release

--- scripts/build.sh
+++ scripts/build.sh
@@ -3,13 +3,57 @@
 set -e
 set -o pipefail
 set -x
-
-SCRIPT_DIRECTORY="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
-ROOT_DIRECTORY="${SCRIPT_DIRECTORY}/.."
+set -u
 
 # TODO: Re-enable test builds if possible using a locally generated signing key.
 
-IPHONE_DESTINATION="platform=iOS Simulator,name=iPhone 12 Pro,OS=14.4"
+SCRIPT_DIRECTORY="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+
+ROOT_DIRECTORY="${SCRIPT_DIRECTORY}/.."
+BUILD_DIRECTORY="${ROOT_DIRECTORY}/build"
+TEMPORARY_DIRECTORY="${ROOT_DIRECTORY}/temp"
+
+KEYCHAIN_PATH="${TEMPORARY_DIRECTORY}/temporary.keychain"
+ARCHIVE_PATH="${BUILD_DIRECTORY}/Bookmarks.xcarchive"
+FASTLANE_ENV_PATH="${ROOT_DIRECTORY}/fastlane/.env"
+
+CHANGES_SCRIPT="${ROOT_DIRECTORY}/changes/changes"
+
+# Process the command line arguments.
+POSITIONAL=()
+NOTARIZE=true
+RELEASE=false
+while [[ $# -gt 0 ]]
+do
+    key="$1"
+    case $key in
+        -N|--skip-notarize)
+        NOTARIZE=false
+        shift
+        ;;
+        -r|--release)
+        RELEASE=true
+        shift
+        ;;
+        *)
+        POSITIONAL+=("$1")
+        shift
+        ;;
+    esac
+done
+
+# iPhone to be used for smoke test builds and tests.
+IPHONE_DESTINATION="platform=iOS Simulator,name=iPhone 12 Pro"
+# IPHONE_DESTINATION="platform=iOS Simulator,name=iPhone 12 Pro,OS=14.4"
+
+# Generate a random string to secure the local keychain.
+export TEMPORARY_KEYCHAIN_PASSWORD=`openssl rand -base64 14`
+
+# Source the Fastlane .env file if it exists to make local development easier.
+if [ -f "$FASTLANE_ENV_PATH" ] ; then
+    echo "Sourcing .env..."
+    source "$FASTLANE_ENV_PATH"
+fi
 
 function build_scheme {
     xcodebuild \
@@ -22,7 +66,10 @@ function build_scheme {
 
 cd "$ROOT_DIRECTORY"
 
+# List the available schemes.
 xcrun instruments -s devices
+
+# Smoke test builds.
 
 # BookmarksCore
 build_scheme "BookmarksCore iOS" clean build build-for-testing test \
@@ -37,3 +84,59 @@ build_scheme "Bookmarks iOS" clean build build-for-testing test \
 
 # macOS
 build_scheme "Bookmarks macOS" clean build build-for-testing
+
+# Build the macOS archive.
+
+# Clean up the build directory.
+if [ -d "$BUILD_DIRECTORY" ] ; then
+    rm -r "$BUILD_DIRECTORY"
+fi
+mkdir -p "$BUILD_DIRECTORY"
+
+# Create the a new keychain.
+if [ -d "$TEMPORARY_DIRECTORY" ] ; then
+    rm -rf "$TEMPORARY_DIRECTORY"
+fi
+mkdir -p "$TEMPORARY_DIRECTORY"
+security create-keychain -p "$TEMPORARY_KEYCHAIN_PASSWORD" "$KEYCHAIN_PATH"
+security set-keychain-settings -lut 21600 "$KEYCHAIN_PATH"
+
+function cleanup {
+  # Cleanup the temporary files and keychain.
+  rm -rf "$TEMPORARY_DIRECTORY"
+}
+
+trap cleanup EXIT
+
+# Determine the version and build number.
+VERSION_NUMBER=`"$CHANGES_SCRIPT" --scope macOS current-version`
+GIT_COMMIT=`git rev-parse --short HEAD`
+TIMESTAMP=`date +%s`
+BUILD_NUMBER="${GIT_COMMIT}.${TIMESTAMP}"
+
+# Import the certificates into our dedicated keychain.
+fastlane import_certificates keychain:"$KEYCHAIN_PATH"
+security unlock-keychain -p "$TEMPORARY_KEYCHAIN_PASSWORD" "$KEYCHAIN_PATH"
+security list-keychain -d user -s "$KEYCHAIN_PATH"
+
+# Archive and export the build.
+xcodebuild -workspace Bookmarks.xcworkspace -scheme "Bookmarks macOS" -config Release -archivePath "$ARCHIVE_PATH" OTHER_CODE_SIGN_FLAGS="--keychain=\"${KEYCHAIN_PATH}\"" BUILD_NUMBER=$BUILD_NUMBER MARKETING_VERSION=$VERSION_NUMBER archive | xcpretty
+xcodebuild -archivePath "$ARCHIVE_PATH" -exportArchive -exportPath "$BUILD_DIRECTORY" -exportOptionsPlist "macos/ExportOptions.plist"
+
+APP_BASENAME="Bookmarks.app"
+APP_PATH="$BUILD_DIRECTORY/$APP_BASENAME"
+
+# Show the code signing details.
+codesign -dvv "$APP_PATH"
+
+# Notarize the release build.
+if $NOTARIZE ; then
+    fastlane notarize_release package:"$APP_PATH"
+fi
+
+# Archive the results.
+pushd "$BUILD_DIRECTORY"
+zip -r "Bookmarks-macOS-${VERSION_NUMBER}.zip" "$APP_BASENAME"
+rm -r "$APP_BASENAME"
+zip -r "Artifacts.zip" "."
+popd

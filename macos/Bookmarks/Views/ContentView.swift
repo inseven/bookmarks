ffbaca990f50c9f1303a6b196b805b0d592a2b1d
Jason Barrie Morley <jbmorley@mac.com>
2021-09-14 14:34:50 -0700

    Add experimental selection code.

--- macos/Bookmarks/Views/ContentView.swift
+++ macos/Bookmarks/Views/ContentView.swift
@@ -24,6 +24,406 @@ import SwiftUI
 import BookmarksCore
 import Interact
 
+extension NSView {
+
+    func subviews(matching matcher: (NSView) -> Bool) -> [NSView] {
+        var result: [NSView] = []
+        for view in subviews {
+            if matcher(view) {
+                result.append(view)
+            }
+            result = result + view.subviews(matching: matcher)
+        }
+        return result
+    }
+
+}
+
+struct RelativeView {
+
+    let view: SelectableMarker.SelectableMarkerView
+    let frame: CGRect
+
+    var xRange: Range<CGFloat> {
+        Range(uncheckedBounds: (frame.origin.x, frame.origin.x + frame.size.width))
+    }
+
+    var yRange: Range<CGFloat> {
+        Range(uncheckedBounds: (frame.origin.y, frame.origin.y + frame.size.height))
+    }
+
+
+}
+
+struct InjectionView <Content: View>: NSViewRepresentable {
+
+    class InjectionHostingView: NSHostingView <Content> {
+
+        required init(rootView: Content) {
+            super.init(rootView: rootView)
+        }
+
+        @MainActor @objc required dynamic init?(coder aDecoder: NSCoder) {
+            fatalError("init(coder:) has not been implemented")
+        }
+
+        override var acceptsFirstResponder: Bool { true }
+
+        override func mouseDown(with event: NSEvent) {
+            // TODO: Consider passing on the mouse down event?
+            window?.makeFirstResponder(self)
+        }
+
+        override func keyDown(with event: NSEvent) {
+            let character = event.characters?.first
+            switch character {
+            case KeyEquivalent.downArrow.character:
+                select(direction: .down)
+            case KeyEquivalent.upArrow.character:
+                select(direction: .up)
+            case KeyEquivalent.leftArrow.character:
+                select(direction: .left)
+            case KeyEquivalent.rightArrow.character:
+                select(direction: .right)
+            default:
+                super.keyDown(with: event)
+            }
+        }
+
+        // TODO: This view could store the known selection?
+
+        func select(direction: MoveCommandDirection) {
+            // TODO: The views should probably not descend into other selectable regions to avoid overlap?
+            // TODO: Do I need some sort of proxy for the focusableSection?
+            guard let views = subviews(matching: { type(of: $0) == SelectableMarker.SelectableMarkerView.self }) as? [SelectableMarker.SelectableMarkerView] else {
+                print("no selectable views")
+                return
+            }
+
+            print(direction)
+
+            let relativeViews = views.map { RelativeView(view: $0, frame: convert($0.frame, from: $0)) }
+
+            guard let focusedView = relativeViews.first(where: { $0.view.hasSelectionFocus.wrappedValue }) else {
+                views.first?.focus()
+                return
+            }
+
+            // Filter the views by views in the same plane.
+            var candidateViews: [RelativeView] = []
+            switch direction {
+            case .up, .down:
+                candidateViews = relativeViews.filter { focusedView.xRange.overlaps($0.xRange) }
+            case .left, .right:
+                candidateViews = relativeViews.filter { focusedView.yRange.overlaps($0.yRange) }
+            @unknown default:
+                print("unknown direction")
+            }
+
+            var sortedViews: [RelativeView] = []
+            switch direction {
+            case .up:
+                sortedViews = candidateViews.sorted(by: { $0.frame.origin.y < $1.frame.origin.y }).reversed()
+            case .down:
+                sortedViews = candidateViews.sorted(by: { $0.frame.origin.y < $1.frame.origin.y })
+            case .left:
+                sortedViews = candidateViews.sorted(by: { $0.frame.origin.x < $1.frame.origin.x }).reversed()
+            case .right:
+                sortedViews = candidateViews.sorted(by: { $0.frame.origin.x < $1.frame.origin.x })
+            @unknown default:
+                print("unknown direction")
+            }
+
+            // Print the frames.
+            for view in sortedViews {
+                print("frame = \(view.frame)")
+            }
+
+            if let index = sortedViews.firstIndex(where: { $0.view.hasSelectionFocus.wrappedValue }) {
+                print("index = \(index)")
+                if index < sortedViews.count - 1 {
+                    sortedViews[index].view.unfocus()
+                    sortedViews[index + 1].view.focus()
+                }
+            } else {
+                sortedViews.first?.view.focus()
+            }
+
+
+        }
+
+    }
+
+    class Coordinator: NSObject {
+
+        var parent: InjectionView
+
+        init(_ parent: InjectionView) {
+            self.parent = parent
+        }
+
+    }
+
+    var content: () -> Content
+
+    init(@ViewBuilder content: @escaping () -> Content) {
+        self.content = content
+    }
+
+    func makeCoordinator() -> Coordinator {
+        Coordinator(self)
+    }
+
+    func makeNSView(context: Context) -> InjectionHostingView {
+        return InjectionHostingView(rootView: content())
+    }
+
+    func updateNSView(_ view: InjectionHostingView, context: NSViewRepresentableContext<InjectionView>) {
+    }
+
+}
+
+struct LayerView: NSViewRepresentable {
+
+    class InjectionHostingView: NSView {
+
+        required init() {
+            super.init(frame: .zero)
+            self.wantsLayer = true
+            self.layer?.backgroundColor = CGColor(red: 1, green: 0, blue: 1, alpha: 0.2)
+        }
+
+        @MainActor @objc required dynamic init?(coder aDecoder: NSCoder) {
+            fatalError("init(coder:) has not been implemented")
+        }
+
+        override var acceptsFirstResponder: Bool { true }
+
+        override func mouseDown(with event: NSEvent) {
+            // TODO: Consider passing on the mouse down event?
+            window?.makeFirstResponder(self)
+        }
+
+        override func keyDown(with event: NSEvent) {
+            let character = event.characters?.first
+            switch character {
+            case KeyEquivalent.downArrow.character:
+                select(direction: .down)
+            case KeyEquivalent.upArrow.character:
+                select(direction: .up)
+            case KeyEquivalent.leftArrow.character:
+                select(direction: .left)
+            case KeyEquivalent.rightArrow.character:
+                select(direction: .right)
+            default:
+                super.keyDown(with: event)
+            }
+        }
+
+        // TODO: This view could store the known selection?
+
+        func select(direction: MoveCommandDirection) {
+            // TODO: The views should probably not descend into other selectable regions to avoid overlap?
+            // TODO: Do I need some sort of proxy for the focusableSection?
+
+            guard let superview = superview?.superview else {
+                return
+            }
+
+            guard let views = superview.subviews(matching: { type(of: $0) == SelectableMarker.SelectableMarkerView.self }) as? [SelectableMarker.SelectableMarkerView] else {
+                print("no selectable views")
+                return
+            }
+
+            print(direction)
+
+            let relativeViews = views.map { RelativeView(view: $0, frame: convert($0.frame, from: $0)) }
+
+            guard let focusedView = relativeViews.first(where: { $0.view.hasSelectionFocus.wrappedValue }) else {
+                views.first?.focus()
+                return
+            }
+
+            // Filter the views by views in the same plane.
+            var candidateViews: [RelativeView] = []
+            switch direction {
+            case .up, .down:
+                candidateViews = relativeViews.filter { focusedView.xRange.overlaps($0.xRange) }
+            case .left, .right:
+                candidateViews = relativeViews.filter { focusedView.yRange.overlaps($0.yRange) }
+            @unknown default:
+                print("unknown direction")
+            }
+
+            var sortedViews: [RelativeView] = []
+            switch direction {
+            case .up:
+                sortedViews = candidateViews.sorted(by: { $0.frame.origin.y < $1.frame.origin.y })
+            case .down:
+                sortedViews = candidateViews.sorted(by: { $0.frame.origin.y < $1.frame.origin.y }).reversed()
+            case .left:
+                sortedViews = candidateViews.sorted(by: { $0.frame.origin.x < $1.frame.origin.x }).reversed()
+            case .right:
+                sortedViews = candidateViews.sorted(by: { $0.frame.origin.x < $1.frame.origin.x })
+            @unknown default:
+                print("unknown direction")
+            }
+
+            // Print the frames.
+            for view in sortedViews {
+                print("frame = \(view.frame)")
+            }
+
+            if let index = sortedViews.firstIndex(where: { $0.view.hasSelectionFocus.wrappedValue }) {
+                print("index = \(index)")
+                if index < sortedViews.count - 1 {
+                    sortedViews[index].view.unfocus()
+                    sortedViews[index + 1].view.focus()
+                }
+            } else {
+                sortedViews.first?.view.focus()
+            }
+
+
+        }
+
+    }
+
+    class Coordinator: NSObject {
+
+        var parent: LayerView
+
+        init(_ parent: LayerView) {
+            self.parent = parent
+        }
+
+    }
+
+    func makeCoordinator() -> Coordinator {
+        Coordinator(self)
+    }
+
+    func makeNSView(context: Context) -> InjectionHostingView {
+        return InjectionHostingView()
+    }
+
+    func updateNSView(_ view: InjectionHostingView, context: NSViewRepresentableContext<LayerView>) {
+    }
+
+}
+
+struct SelectableMarker: NSViewRepresentable {
+
+    @Binding var hasSelectionFocus: Bool
+
+    class SelectableMarkerView: NSView {
+
+        let hasSelectionFocus: Binding<Bool>
+
+        init(hasSelectionFocus: Binding<Bool>) {
+            self.hasSelectionFocus = hasSelectionFocus
+            super.init(frame: .zero)
+        }
+
+        required init?(coder: NSCoder) {
+            fatalError("init(coder:) has not been implemented")
+        }
+
+        func focus() {
+            hasSelectionFocus.wrappedValue = true
+        }
+
+        func unfocus() {
+            hasSelectionFocus.wrappedValue = false
+        }
+
+    }
+
+    class Coordinator: NSObject {
+
+        var parent: SelectableMarker
+
+        init(_ parent: SelectableMarker) {
+            self.parent = parent
+        }
+
+    }
+
+    func makeCoordinator() -> Coordinator {
+        Coordinator(self)
+    }
+
+    func makeNSView(context: Context) -> SelectableMarkerView {
+        return SelectableMarkerView(hasSelectionFocus: $hasSelectionFocus)
+    }
+
+    func updateNSView(_ view: SelectableMarkerView, context: NSViewRepresentableContext<SelectableMarker>) {
+    }
+
+}
+
+
+struct Selectable: ViewModifier {
+
+    @State var hasSelectionFocus: Bool = false
+
+    func body(content: Content) -> some View {
+        content
+            .padding(20)
+            .background(SelectableMarker(hasSelectionFocus: $hasSelectionFocus))
+            .border(hasSelectionFocus ? .red : .secondary, width: 10)
+    }
+
+}
+
+extension View {
+
+    public func selectable() -> some View {
+        modifier(Selectable())
+    }
+
+}
+
+
+struct ExampleView: View {
+
+    var body: some View {
+        InjectionView {
+            HStack {
+                VStack {
+                    Text("Hello")
+                        .selectable()
+                    Text("Goodbye")
+                        .selectable()
+                }
+                Text("Random")
+                    .selectable()
+            }
+        }
+    }
+
+}
+
+struct ExampleView2: View {
+
+    var body: some View {
+        ZStack {
+            HStack {
+                VStack {
+                    Text("Hello")
+                        .selectable()
+                    Text("Goodbye")
+                        .selectable()
+                }
+                Text("Random")
+                    .selectable()
+            }
+            LayerView()
+        }
+    }
+
+}
+
 struct ContentView: View {
 
     @Environment(\.manager) var manager
@@ -119,6 +519,7 @@ struct ContentView: View {
             }
             .background(Color(NSColor.textBackgroundColor))
             .overlay(bookmarksView.state == .loading ? LoadingView() : nil)
+            .overlay(ExampleView2())
         }
         .onAppear {
             bookmarksView.start()

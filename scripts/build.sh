67b507b49b406bd024a09207010ec04a40b68e40
Jason Barrie Morley <jbmorley@mac.com>
2022-07-23 16:29:09 +0100

    Disable the test build

--- scripts/build.sh
+++ scripts/build.sh
@@ -105,19 +105,19 @@ xcode_project -list
 
 # Smoke test builds.
 
-# iOS
-sudo xcode-select --switch "$IOS_XCODE_PATH"
-build_scheme "BookmarksCore iOS" clean build build-for-testing test \
-    -sdk iphonesimulator \
-    -destination "$IPHONE_DESTINATION"
-build_scheme "Bookmarks iOS" clean build build-for-testing test \
-    -sdk iphonesimulator \
-    -destination "$IPHONE_DESTINATION"
-
-# macOS
-sudo xcode-select --switch "$MACOS_XCODE_PATH"
-build_scheme "BookmarksCore macOS" clean build build-for-testing test
-build_scheme "Bookmarks macOS" clean build build-for-testing
+# # iOS
+# sudo xcode-select --switch "$IOS_XCODE_PATH"
+# build_scheme "BookmarksCore iOS" clean build build-for-testing test \
+#     -sdk iphonesimulator \
+#     -destination "$IPHONE_DESTINATION"
+# build_scheme "Bookmarks iOS" clean build build-for-testing test \
+#     -sdk iphonesimulator \
+#     -destination "$IPHONE_DESTINATION"
+#
+# # macOS
+# sudo xcode-select --switch "$MACOS_XCODE_PATH"
+# build_scheme "BookmarksCore macOS" clean build build-for-testing test
+# build_scheme "Bookmarks macOS" clean build build-for-testing
 
 # Clean up the build directory.
 if [ -d "$BUILD_DIRECTORY" ] ; then

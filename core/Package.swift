5c74a3fcfd39c33f159efa9166988f81e3044235
Jason Barrie Morley <jbmorley@mac.com>
2022-12-23 14:32:14 +0000

    refactor: Convert `BookmarksCore` to a Swift package

---
+++ core/Package.swift
@@ -0,0 +1,38 @@
+// swift-tools-version: 5.7
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "BookmarksCore",
+    platforms: [
+        .iOS(.v16),
+        .macOS(.v13)
+    ],
+    products: [
+        // Products define the executables and libraries a package produces, and make them visible to other packages.
+        .library(
+            name: "BookmarksCore",
+            targets: ["BookmarksCore"]),
+    ],
+    dependencies: [
+        .package(path: "./../SQLite.swift"),
+        .package(path: "./../ios/hpple"),
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
+        // Targets can depend on other targets in this package, and on products in packages this package depends on.
+        .target(
+            name: "BookmarksCore",
+            dependencies: [
+                .product(name: "SQLite", package: "SQLite.swift"),
+                .product(name: "TFHpple", package: "hpple"),
+            ],
+            resources: [
+                .process("Licenses"),
+            ]),
+        .testTarget(
+            name: "BookmarksCoreTests",
+            dependencies: ["BookmarksCore"]),
+    ]
+)

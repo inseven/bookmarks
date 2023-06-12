984bfd6d4351b18c4e0557a67f1b6f9f3386b668
Jason Morley <hello@jbmorley.co.uk>
2023-06-12 14:26:37 -0400

    fix: Show placeholder text when there are no bookmarks (#582)

--- ios/Bookmarks/Views/SectionView.swift
+++ ios/Bookmarks/Views/SectionView.swift
@@ -61,6 +61,7 @@ struct SectionView: View {
             }
 
         }
+        .overlay(sectionViewModel.bookmarks.isEmpty ? PlaceholderView("No Bookmarks") : nil)
         .overlay(sectionViewModel.state == .loading ? LoadingView() : nil)
         .navigationTitle(sectionViewModel.title)
         .navigationBarTitleDisplayMode(.inline)
